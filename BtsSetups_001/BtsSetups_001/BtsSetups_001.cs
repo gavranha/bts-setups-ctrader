35b99f61ca838bd413f7b4a0379b510445f2b9a8
JS Lopes <gavranha@gmail.com>
2023-05-09 20:30:02 -0400

    implements EMA crossover with user config params

--- BtsSetups_001/BtsSetups_001/BtsSetups_001.cs
+++ BtsSetups_001/BtsSetups_001/BtsSetups_001.cs
@@ -12,25 +12,43 @@ namespace cAlgo.Robots
     [Robot(AccessRights = AccessRights.None)]
     public class BtsSetups_001 : Robot
     {
-        [Parameter(DefaultValue = "Hello world!")]
-        public string Message { get; set; }
+        [Parameter("Slow EMA period", DefaultValue = 50, MinValue = 2, Step = 1)]
+        public int SlowEmaPeriod { get; set; }
+        [Parameter("Fast EMA period", DefaultValue = 9, MinValue = 2, Step = 1)]
+        public int FastEmaPeriod { get; set; }
+
+        private DataSeries EmaSource;
+        private ExponentialMovingAverage SlowEma;
+        private ExponentialMovingAverage FastEma;
 
         protected override void OnStart()
         {
-            // To learn more about cTrader Automate visit our Help Center:
-            // https://help.ctrader.com/ctrader-automate
-
-            Print(Message);
+            SlowEma = Indicators.ExponentialMovingAverage(EmaSource, SlowEmaPeriod);
+            FastEma = Indicators.ExponentialMovingAverage(EmaSource, FastEmaPeriod);
         }
 
-        protected override void OnTick()
+        protected override void OnBar()
         {
-            // Handle price updates here
+            base.OnBar();
+
+            if (HasBuySignal())
+            {
+                Print($"HasBuySignal {HasBuySignal()}");
+            }
+            else if (HasSellSignal())
+            {
+                Print($"HasSellSignal {HasSellSignal()}");
+            }
         }
 
-        protected override void OnStop()
+        private bool HasSellSignal()
         {
-            // Handle cBot stop here
+            return FastEma.Result.Last(2) >= SlowEma.Result.Last(2) && FastEma.Result.Last(1) < SlowEma.Result.Last(1);
+        }
+
+        private bool HasBuySignal()
+        {
+            return FastEma.Result.Last(2) <= SlowEma.Result.Last(2) && FastEma.Result.Last(1) > SlowEma.Result.Last(1);
         }
     }
 }
